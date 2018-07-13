20caef141a711f67820d7d759c5a7f0e275e6d21
Ken C <kenchoi826@gmail.com>
2018-07-12 23:21:10 -0700

    included ORM

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,21 @@
 from app import app
-from flask import render_template, redirect, flash, url_for
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username':'Ken'}
-    title = 'Home'
-    posts =[
-        {
-            'author':{'username':'Ken'},
-            'blog':'Hello Yellow'
-        },
-        {
-            'author':{'username':'Jack'},
-            'blog':'Hello Bellow'
-        }
-    ]
+    return render_template('index.html')
 
-    return render_template('index.html',user=user,title=title, posts=posts)
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me{}'.format(form.username.data, form.remember_me.data))
+        flash(
+            f'Login requested for user: {form.username.data}, remember me\
+            : {form.remember_me.data}')
         return redirect(url_for('index'))
-    return render_template('login.html',form=form, title='Sign in')
+    return render_template('login.html', title='Sign In', form=form)
